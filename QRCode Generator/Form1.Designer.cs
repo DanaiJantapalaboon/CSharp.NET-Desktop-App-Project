d051cf80acf7038bceda35c456ae46b4a0b5f860
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2022-01-30 12:52:53 +0700

    Add files via upload

---
+++ QRCode Generator/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿
+namespace QRCode_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QRBox = new System.Windows.Forms.PictureBox();
+            this.inputQR = new System.Windows.Forms.TextBox();
+            this.generateQR = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveImage = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clear = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.saveHistory = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtidSearch = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.QRBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // QRBox
+            // 
+            this.QRBox.BackColor = System.Drawing.SystemColors.Window;
+            this.QRBox.Location = new System.Drawing.Point(27, 39);
+            this.QRBox.Name = "QRBox";
+            this.QRBox.Size = new System.Drawing.Size(182, 181);
+            this.QRBox.TabIndex = 0;
+            this.QRBox.TabStop = false;
+            // 
+            // inputQR
+            // 
+            this.inputQR.Location = new System.Drawing.Point(28, 251);
+            this.inputQR.Name = "inputQR";
+            this.inputQR.Size = new System.Drawing.Size(470, 27);
+            this.inputQR.TabIndex = 1;
+            // 
+            // generateQR
+            // 
+            this.generateQR.Location = new System.Drawing.Point(238, 123);
+            this.generateQR.Name = "generateQR";
+            this.generateQR.Size = new System.Drawing.Size(146, 56);
+            this.generateQR.TabIndex = 2;
+            this.generateQR.Text = "Generate QR Code";
+            this.generateQR.UseVisualStyleBackColor = true;
+            this.generateQR.Click += new System.EventHandler(this.generateQR_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1286, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "QR Code Generator v1.01";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input Link or Text...";
+            // 
+            // saveImage
+            // 
+            this.saveImage.Location = new System.Drawing.Point(238, 88);
+            this.saveImage.Name = "saveImage";
+            this.saveImage.Size = new System.Drawing.Size(146, 29);
+            this.saveImage.TabIndex = 2;
+            this.saveImage.Text = "Save Image";
+            this.saveImage.UseVisualStyleBackColor = true;
+            this.saveImage.Click += new System.EventHandler(this.saveImage_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1482, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveImageToolStripMenuItem,
+            this.saveDataToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveImageToolStripMenuItem
+            // 
+            this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(178, 26);
+            this.saveImageToolStripMenuItem.Text = "Save Image...";
+            this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.saveImageToolStripMenuItem_Click);
+            // 
+            // saveDataToolStripMenuItem
+            // 
+            this.saveDataToolStripMenuItem.Name = "saveDataToolStripMenuItem";
+            this.saveDataToolStripMenuItem.Size = new System.Drawing.Size(178, 26);
+            this.saveDataToolStripMenuItem.Text = "Save Data";
+            this.saveDataToolStripMenuItem.Click += new System.EventHandler(this.saveDataToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(178, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(28, 337);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(146, 29);
+            this.clear.TabIndex = 5;
+            this.clear.Text = "Clear Text";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(238, 218);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(260, 27);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(238, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Select Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(526, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "History";
+            // 
+            // saveHistory
+            // 
+            this.saveHistory.Location = new System.Drawing.Point(238, 55);
+            this.saveHistory.Name = "saveHistory";
+            this.saveHistory.Size = new System.Drawing.Size(146, 29);
+            this.saveHistory.TabIndex = 8;
+            this.saveHistory.Text = "Save Data";
+            this.saveHistory.UseVisualStyleBackColor = true;
+            this.saveHistory.Click += new System.EventHandler(this.saveHistory_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(524, 341);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "id Search";
+            // 
+            // txtidSearch
+            // 
+            this.txtidSearch.Location = new System.Drawing.Point(600, 338);
+            this.txtidSearch.Name = "txtidSearch";
+            this.txtidSearch.Size = new System.Drawing.Size(125, 27);
+            this.txtidSearch.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(731, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(831, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(28, 304);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(470, 27);
+            this.txtNote.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Note...";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(526, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(934, 276);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(931, 337);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(94, 29);
+            this.btnRefresh.TabIndex = 14;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1482, 378);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtidSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.saveHistory);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveImage);
+            this.Controls.Add(this.generateQR);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.inputQR);
+            this.Controls.Add(this.QRBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(1500, 425);
+            this.MinimumSize = new System.Drawing.Size(1500, 425);
+            this.Name = "Form1";
+            this.Text = "QR Code Generator v1.01";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.QRBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox QRBox;
+        private System.Windows.Forms.TextBox inputQR;
+        private System.Windows.Forms.Button generateQR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button saveImage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button saveHistory;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtidSearch;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem saveDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ QRCode Generator/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace QRCode_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updateGridView();
+        }
+
+        MySqlConnection connect = new MySqlConnection("host = localhost; user = root; password = 1234; database = qrcodegen");
+        MySqlCommand command;
+
+        private void updateGridView()
+        {
+            try
+            {
+                string viewData = "SELECT * FROM qrcodegen.tblqrgen";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(viewData, connect);
+
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            catch
+            {
+                MessageBox.Show("ไม่สามารถดึงข้อมูลจากฐานข้อมูลได้ กรุณาตรวจสอบการเชื่อมต่อ", "Notice",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+            }
+        }
+
+        private void executeQuery(string query)
+        {
+            try
+            {
+                connect.Open();
+                command = new MySqlCommand(query, connect);
+
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    inputQR.Clear();
+                    txtNote.Clear();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                connect.Close();
+            }
+        }
+
+        private void saveData()
+        {
+            DateTime myDateTime = dateTimePicker1.Value;
+            string sqlDate = myDateTime.ToString("yyyy-MM-dd");
+
+            string save = "INSERT INTO qrcodegen.tblqrgen (date, Note, Link) " +
+                          "VALUES ('" + sqlDate + "', '" + txtNote.Text + "', '" + inputQR.Text + "')";
+
+            executeQuery(save);
+            updateGridView();
+        }
+
+        private void saveHistory_Click(object sender, EventArgs e)
+        {
+            saveData();
+        }
+        private void saveDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveData();
+        }
+
+        private void generateQR_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw qrCode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            QRBox.Image = qrCode.Draw(inputQR.Text, 50);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string resultText = "C# using Zen.Barcode.Rendering.Framework -Version 3.1.10729.1\nUI Designed & Programming by Danai J.";
+            MessageBox.Show(resultText, "About QR Code Generator v1.01");
+        }
+
+        //โค้ด Save Image จาก pictureBox
+        //link..https://stackoverflow.com/questions/21418238/c-save-image-on-picturebox-into-folder-on-my-computer
+        private void saveImageToolStripMenuItem_Click(object sender, EventArgs e)   //Save Image จาก Menu FileSave
+        {
+            SaveImageCapture(QRBox.Image);
+        }
+
+        private void saveImage_Click(object sender, EventArgs e)    //Save Image จาก Button
+        {
+            SaveImageCapture(QRBox.Image);
+        }
+
+        private void SaveImageCapture(System.Drawing.Image image) //ส่วนทำงาน Save Image
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.FileName = "Image";
+            s.DefaultExt = ".Jpg";
+            s.Filter = "Image (.jpg)|*.jpg";
+
+            s.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            s.RestoreDirectory = true;
+            if (s.ShowDialog() == DialogResult.OK)
+            {
+                string filename = s.FileName;
+                using (System.IO.FileStream fstream = new System.IO.FileStream(filename, System.IO.FileMode.Create))
+                {
+                    image.Save(fstream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    fstream.Close();
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = new DialogResult();
+            dialog = MessageBox.Show("Do you want to exit?", "QR Code Generator", MessageBoxButtons.OKCancel);
+            if (dialog == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            inputQR.Clear();
+            txtNote.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string searchValue = txtidSearch.Text;
+            dataGridView1.ClearSelection();
+
+            try
+            {
+                bool valueResult = false;
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    for (int i = 0; i < row.Cells.Count; i++)
+                    {
+                        if (row.Cells[i].Value != null && row.Cells[i].Value.ToString().Equals(searchValue))
+                        {
+                            int rowIndex = row.Index;
+                            dataGridView1.Rows[rowIndex].Selected = true;
+                            valueResult = true;
+                            txtidSearch.Clear();
+                            break;
+                        }
+                    }
+                }
+                if (!valueResult)
+                {
+                    MessageBox.Show("Unable to find " + txtidSearch.Text, "Not Found");
+                    return;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtidSearch.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtNote.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            inputQR.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string delete = "DELETE FROM qrcodegen.tblqrgen WHERE id = '"+ txtidSearch.Text +"'";
+            executeQuery(delete);
+            updateGridView();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            updateGridView();
+        }
+    }
+}
